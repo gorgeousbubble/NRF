e31adde68cd394cd8e9955311e044fb21d361843
Alopex6414 <1029535012@qq.com>
2025-03-28 21:39:22 +0800

    Add BenchmarkHandleNFRegisterParallel parallel unit test function

--- app/nf_management_test.go
+++ app/nf_management_test.go
@@ -102,7 +102,6 @@ func BenchmarkHandleNFRegister(b *testing.B) {
 	// start benchmark test...
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		b.StartTimer()
 		// construct network function request content
 		rand.New(rand.NewSource(time.Now().UnixNano()))
 		url := server.URL + "/nnrf-nfm/v1/nf-instances"
@@ -139,8 +138,61 @@ func BenchmarkHandleNFRegister(b *testing.B) {
 		assert.Equal(b, nfInstanceId, response.NFInstanceId)
 		assert.Equal(b, nfType, response.NFType)
 		assert.Equal(b, nfStatus, response.NFStatus)
-		b.StopTimer()
 	}
+}
+
+func BenchmarkHandleNFRegisterParallel(b *testing.B) {
+	// initialize NRF Service
+	NRFService = New()
+	err := NRFService.Init()
+	if err != nil {
+		b.Error(err)
+	}
+	// start http test service
+	server, router := startTestServer()
+	defer server.Close()
+	// start benchmark test...
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			// construct network function request content
+			rand.New(rand.NewSource(time.Now().UnixNano()))
+			url := server.URL + "/nnrf-nfm/v1/nf-instances"
+			nfInstanceId := uuid.New().String()
+			nfType := NetworkFunctionType[rand.Intn(len(NetworkFunctionType))]
+			nfStatus := NetworkFunctionStatus[rand.Intn(len(NetworkFunctionStatus))]
+			// assemble network function http request
+			profile := NFProfile{
+				NFInstanceId: nfInstanceId,
+				NFType:       nfType,
+				NFStatus:     nfStatus,
+			}
+			body, err := json.Marshal(profile)
+			if err != nil {
+				b.Errorf("Error marshalling profile: %v", err)
+			}
+			// http request NFRegister
+			w := httptest.NewRecorder()
+			request, err := http.NewRequest("PUT", url+"/"+nfInstanceId, bytes.NewReader(body))
+			if err != nil {
+				b.Errorf("Error creating request: %v", err)
+			}
+			request.Header.Set("Content-Type", "application/json")
+			router.ServeHTTP(w, request)
+			var response NFProfile
+			err = json.Unmarshal(w.Body.Bytes(), &response)
+			if err != nil {
+				b.Errorf("Error unmarshalling response: %v", err)
+			}
+			// assert http response
+			assert.Equal(b, http.StatusCreated, w.Code)
+			assert.Equal(b, "application/json", w.Header().Get("Content-Type"))
+			assert.Equal(b, url+"/"+nfInstanceId, w.Header().Get("Location"))
+			assert.Equal(b, nfInstanceId, response.NFInstanceId)
+			assert.Equal(b, nfType, response.NFType)
+			assert.Equal(b, nfStatus, response.NFStatus)
+		}
+	})
 }
 
 func TestHandleNFRegisterWithUpperNFInstanceID(t *testing.T) {
