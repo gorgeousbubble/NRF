2b4f7aaf308b0405c474d2379412875058fa36f3
Alopex6414 <1029535012@qq.com>
2025-05-07 21:48:14 +0800

    Add FuzzHandleNFRegisterWithUpperNFInstanceID fuzz unit test case

--- app/nf_management_test.go
+++ app/nf_management_test.go
@@ -431,6 +431,63 @@ func BenchmarkHandleNFRegisterWithUpperNFInstanceIDParallel(b *testing.B) {
 	})
 }
 
+func FuzzHandleNFRegisterWithUpperNFInstanceID(f *testing.F) {
+	// initialize NRF Service
+	NRFService = New()
+	err := NRFService.Init()
+	if err != nil {
+		f.Error(err)
+	}
+	// start http test service
+	server, router := startTestServer()
+	defer server.Close()
+	// construct network function request content
+	url := server.URL + "/nnrf-nfm/v1/nf-instances"
+	nfInstanceId := strings.ToUpper(uuid.New().String())
+	nfType := "UPF"
+	nfStatus := "REGISTERED"
+	// assemble network function http request
+	profile := NFProfile{
+		NFInstanceId: nfInstanceId,
+		NFType:       nfType,
+		NFStatus:     nfStatus,
+	}
+	body, err := json.Marshal(profile)
+	if err != nil {
+		f.Errorf("Error marshalling profile: %v", err)
+	}
+	// add fuzzy test database
+	f.Add(body)
+	f.Fuzz(func(t *testing.T, body []byte) {
+		// unexpected result
+		defer func() {
+			if r := recover(); r != nil {
+				t.Errorf("panic: %v", r)
+			}
+		}()
+		// http request NFRegister
+		w := httptest.NewRecorder()
+		request, err := http.NewRequest("PUT", url+"/"+nfInstanceId, bytes.NewReader(body))
+		if err != nil {
+			t.Errorf("Error creating request: %v", err)
+		}
+		request.Header.Set("Content-Type", "application/json")
+		router.ServeHTTP(w, request)
+		var response NFProfile
+		err = json.Unmarshal(w.Body.Bytes(), &response)
+		if err != nil {
+			t.Errorf("Error unmarshalling response: %v", err)
+		}
+		// assert http response
+		assert.Equal(t, http.StatusCreated, w.Code)
+		assert.Equal(t, "application/json", w.Header().Get("Content-Type"))
+		assert.Equal(t, url+"/"+strings.ToLower(nfInstanceId), w.Header().Get("Location"))
+		assert.Equal(t, strings.ToLower(nfInstanceId), response.NFInstanceId)
+		assert.Equal(t, nfType, response.NFType)
+		assert.Equal(t, nfStatus, response.NFStatus)
+	})
+}
+
 func TestHandleNFRegisterWithoutNFType(t *testing.T) {
 	// initialize NRF Service
 	NRFService = New()
